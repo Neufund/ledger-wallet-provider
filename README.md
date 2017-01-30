e6f3f129d93eb9c0591cb823cf86743c91183972
Remco Bloemen <remco-git@xn--2-umb.com>
2017-01-30 09:06:37 +0000

    Check in code

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# ledger-wallet-provider
+# LedgerWalletProvider
+
 Ledger Nano S wallet provider for the Web3 ProviderEngine
+
+# Building
+
+```
+npm install; npm run bundle; npm test
+```
+
+# Using
+
+```
+const Web3 = require('web3');
+const ProviderEngine = require('web3-provider-engine')
+const CacheSubprovider = require('web3-provider-engine/subproviders/cache.js')
+const FixtureSubprovider = require('web3-provider-engine/subproviders/fixture.js')
+const FilterSubprovider = require('web3-provider-engine/subproviders/filters.js')
+const VmSubprovider = require('web3-provider-engine/subproviders/vm.js')
+const NonceSubprovider = require('web3-provider-engine/subproviders/nonce-tracker.js')
+const RpcSubprovider = require('web3-provider-engine/subproviders/rpc.js')
+const LedgerWalletSubprovider = require('ledger-wallet-provider')
+
+var wallet = new LedgerWalletSubprovider()
+
+var engine = new ProviderEngine()
+window.web3 = new Web3(engine)
+
+// static results
+engine.addProvider(new FixtureSubprovider({
+	web3_clientVersion: 'ProviderEngine/v0.0.0/javascript',
+	net_listening: true,
+	eth_hashrate: '0x00',
+	eth_mining: false,
+	eth_syncing: true,
+}))
+
+// cache layer
+engine.addProvider(new CacheSubprovider())
+
+// filters
+engine.addProvider(new FilterSubprovider())
+
+// pending nonce
+engine.addProvider(new NonceSubprovider())
+
+// vm
+engine.addProvider(new VmSubprovider())
+
+// id mgmt
+engine.addProvider(wallet)
+
+// data source
+engine.addProvider(new RpcSubprovider({
+	rpcUrl: node_url,
+}))
+
+// log new blocks
+engine.on('block', function(block){
+	console.log('BLOCK #'+block.number.toString('hex')+' 0x'+block.hash.toString('hex') +'\n')
+})
+
+// network connectivity error
+engine.on('error', function(err){
+	// report connectivity errors
+	console.error(err.stack)
+})
+
+// start polling for blocks
+engine.start()
+
+return web3;
+```
---
+++ src/index.js
@@ -0,0 +1,142 @@
+// Ledger wallet
+//
+// @see https://github.com/MetaMask/provider-engine
+// @see https://github.com/ethereum/wiki/wiki/JavaScript-API
+
+// This is required by the ledger libraries
+if(!window.u2f) {
+	window.u2f = require('../vendor/u2f-api');
+}
+
+const inherits = require('util').inherits
+const HookedWalletSubprovider = require('web3-provider-engine/subproviders/hooked-wallet.js')
+const Ledger3 = require('../vendor/ledger3.js');
+const LedgerEth = require('../vendor/ledger-eth.js');
+const Tx = require('ethereumjs-tx');
+
+module.exports = LedgerWalletSubprovider
+inherits(LedgerWalletSubprovider, HookedWalletSubprovider)
+
+function LedgerWalletSubprovider() {
+	var self = this;
+	LedgerWalletSubprovider.super_.call(self, {getAccounts: ()=>{}})
+	
+	self.path = "44'/60'/1337'";
+	self.accounts = undefined
+	self.scrambleKey = "w0w" // Hardcoded key for the Ledger Nano S
+	self.ledger3 = new Ledger3(self.scrambleKey);
+	self.ledger = new LedgerEth(self.ledger3);
+	
+	window.ledger = self.ledger;
+	
+	self.ledger.getAppConfiguration((config)=>{
+		console.log('Found ledger version:', config.version)
+		// TODO: Need at least version 1.0.4 for EIP155 signing
+	})
+	
+	self.getAccounts = (cb)=>{
+		if(self.accounts !== undefined) {
+			console.log('Using cached result');
+			cb(undefined, self.accounts);
+			return;
+		}
+		
+		const display = true;  // Ask for on-device confirmation
+		const chainCode = false; // Include the chain code
+		console.log('CONFIRM ON LEDGER')
+		self.ledger.getAddress(self.path, (result, error)=>{
+			if(typeof result === undefined) {
+				cb(error, result);
+			}
+			self.accounts = [result.address];
+			cb(error, self.accounts);
+		}, display, chainCode);
+	}
+	
+	self.approveTransaction = (tx, cb)=>{
+		console.log('approveTransaction', tx);
+		// Approved!
+		cb(undefined, true);
+	}
+	
+	self.signTransaction = (txData, callback)=>{
+		console.log('signTransaction', txData);
+		
+		// Encode using ethereumjs-tx
+		var tx = new Tx(txData);
+		
+		// Fetch the chain id
+		web3.version.getNetwork((error, chain_id)=>{
+			if(error) callback(error);
+			
+			// Force chain_id to int
+			chain_id = 0|chain_id
+			
+			// Set the EIP155 bits
+			tx.raw[6] = Buffer.from([chain_id]); // v
+			tx.raw[7] = Buffer.from([]);         // r
+			tx.raw[8] = Buffer.from([]);         // s
+			
+			// TODO: It doesn't seem to increment the nonce value correctly
+			
+			// Encode as hex-rlp for Ledger
+			const hex = tx.serialize().toString('hex')
+			
+			// Pass to ledger for signing
+			self.ledger.signTransaction(self.path, hex, (result, error)=>{
+				if(error) callback(error);
+				
+				// Store signature in transaction
+				tx.v = new Buffer(result.v, 'hex')
+				tx.r = new Buffer(result.r, 'hex')
+				tx.s = new Buffer(result.s, 'hex')
+				
+				// EIP155: v should be chain_id * 2 + {35, 36}
+				const signed_chain_id = Math.floor((tx.v[0] - 35) / 2)
+				if(signed_chain_id !== chain_id) {
+					callback('Invalid signature received. Please update your Ledger Nano S.');
+				}
+				
+				// Return the signed raw transaction
+				var rawTx = '0x' + tx.serialize().toString('hex');
+				callback(undefined, rawTx);
+			})
+		})
+	}
+	
+	self.signMessage = function(msgParams, callback) {
+		console.log('signMessage', msgParams);
+		callback('Error - Signing messages is currently not supported by the Ledger Ethereum App.')
+	}
+}
+
+/*
+0 = ok: Success. Not used in errors but reserved
+1 = other error: An error otherwise not enumerated here
+2 = bad request: The request cannot be processed
+3 = configuration Client configuration is not supported
+4 = device ineligible: The presented device is not eligible for this request. For a registration request this may mean that the token is already registered, and for a sign request it may mean the token does not know the presented key handle.
+5 = timeout: Timeout reached before request could be satisfied
+*/
+
+// Paths:
+// Minimum Nano Ledger S accepts are:
+//
+//  * 44'/60'
+//  * 44'/61'
+//
+// MyEtherWallet.com by default uses the range
+//
+//  * 44'/60'/0'/n
+//
+// Note: no hardend derivation on the `n`
+//
+// BIP44/EIP84 specificies:
+//
+// * m / purpose' / coin_type' / account' / change / address_index 
+//
+// https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki
+// https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+//
+// Implementations:
+// https://github.com/MetaMask/metamask-plugin/blob/master/app/scripts/keyrings/hd.js
