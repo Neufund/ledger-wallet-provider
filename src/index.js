6a7d7dd4629949767ba944343efaa4bdffb2dca9
Tomek Banach <tomek@banciur.org>
2017-12-04 03:46:26 +0100

    Add eslint

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import HookedWalletSubprovider from "web3-provider-engine/subproviders/hooked-wallet";
-import LedgerWallet from "./LedgerWallet";
+import HookedWalletSubprovider from 'web3-provider-engine/subproviders/hooked-wallet';
+import LedgerWallet from './LedgerWallet';
 
-export default async function (getNetworkId, path_override, askForOnDeviceConfirmation) {
-    const ledger = new LedgerWallet(getNetworkId, path_override, askForOnDeviceConfirmation);
-    await ledger.init();
-    const LedgerWalletSubprovider = new HookedWalletSubprovider(ledger);
+export default async function (getNetworkId, pathOverride, askForOnDeviceConfirmation) {
+  const ledger = new LedgerWallet(getNetworkId, pathOverride, askForOnDeviceConfirmation);
+  await ledger.init();
+  const LedgerWalletSubprovider = new HookedWalletSubprovider(ledger);
 
-    // This convenience method lets you handle the case where your users browser doesn't support U2F
-    // before adding the LedgerWalletSubprovider to a providerEngine instance.
-    LedgerWalletSubprovider.isSupported = ledger.isU2FSupported;
-    LedgerWalletSubprovider.ledger = ledger;
+  // This convenience method lets you handle the case where your users browser doesn't support U2F
+  // before adding the LedgerWalletSubprovider to a providerEngine instance.
+  LedgerWalletSubprovider.isSupported = ledger.isU2FSupported;
+  LedgerWalletSubprovider.ledger = ledger;
 
-    return LedgerWalletSubprovider;
-};
+  return LedgerWalletSubprovider;
+}
