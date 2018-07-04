ebba11473db5bd6351f9935a9ac6173f7db2719b
Tomek Banach <tomek@banciur.org>
2018-07-04 14:36:04 +0200

    Allow for BIP44 deviration paths

--- src/LedgerWallet.js
+++ src/LedgerWallet.js
@@ -193,11 +193,11 @@ class LedgerWallet {
    */
   static obtainPathComponentsFromDerivationPath(derivationPath) {
     // check if derivation path follows 44'/60'/x'/n pattern
-    const regExp = /^(44'\/6[0|1]'\/\d+'?\/)(\d+)$/;
+    const regExp = /^(44'\/6[0|1]'\/\d+'?\/(?:[0|1]\/)?)(\d+)$/;
     const matchResult = regExp.exec(derivationPath);
     if (matchResult === null) {
       throw new Error(
-        "To get multiple accounts your derivation path must follow pattern 44'/60|61'/x'/n "
+        "Derivation path must follow pattern 44'/60|61'/x'/n or BIP 44"
       );
     }
 
